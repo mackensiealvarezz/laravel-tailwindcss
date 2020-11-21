57f4d20e745f700b0677129b960840e4cee72544
Mackensie Alvarez <maalvare@estee.com>
2020-11-21 14:36:08 -0500

    components made in tailwind

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<div class=" relative text-gray-700">
+        <div class="container mx-auto flex justify-between px-6 lg:px-0 py-6">
+            <!-- left side of the header -->
+            <div class="flex items-center space-x-4">
+                <a href="#" class="font-bold text-2xl text-teal-800">Beginner tailwind</a>
+                <a href="#" class="hidden lg:inline-block hover:text-gray-800">About</a>
+                <a href="#" class="hidden lg:inline-block hover:text-gray-800">What you will learn</a>
+                <a href="#" class="hidden lg:inline-block hover:text-gray-800">Pricing</a>
+                <a href="#" class="hidden lg:inline-block hover:text-gray-800">FAQ</a>
+            </div>
+
+            <!-- right side of the header -->
+            <div class="flex items-center space-x-2">
+                <a href="#" class="hover:text-gray-800">Login</a>
+                <a href="#" class="hidden lg:inline-block p-3 bg-purple-300 hover:bg-purple-200  text-purple-700 rounded shadow hover:shadow-xl transition duration-500">Get course</a>
+            </div>
+        </div><!-- /.container -->
+    </div>
